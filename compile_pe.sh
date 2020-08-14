aad03be0019496d253f3384e428da3368cfb29ac
Niteshkumar <nitesh156200@gmail.com>
2020-08-15 00:04:03 +0530

    PersonalScripts: PE: Use depth1 for faster cloning!
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- compile_pe.sh
+++ compile_pe.sh
@@ -17,11 +17,11 @@ function syncSource(){
 	echo -e ${blu} "\n[*] Syncing sources... This will take a while [*]" ${txtrst}
 	repo init --depth=1 -u https://github.com/PixelExperience-FanEdition/manifest -b ten-plus
         repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-        git clone https://github.com/niteshkumar2000/android_device_xiaomi_sdm660-common -b ten device/xiaomi/sdm660-common
-        git clone https://github.com/niteshkumar2000/android_device_xiaomi_twolip -b ten device/xiaomi/twolip
-        git clone https://github.com/xiaomi-sdm660/android_kernel_xiaomi_sdm660.git -b master kernel/xiaomi/sdm660
-        git clone https://github.com/niteshkumar2000/proprietary_vendor_xiaomi.git -b ten vendor/xiaomi
-        git clone https://github.com/niteshkumar2000/vendor_MiuiCamera.git vendor/MiuiCamera
+        git clone --depth=1 https://github.com/niteshkumar2000/android_device_xiaomi_sdm660-common -b ten device/xiaomi/sdm660-common
+        git clone --depth=1 https://github.com/niteshkumar2000/android_device_xiaomi_twolip -b ten device/xiaomi/twolip
+        git clone --depth=1 https://github.com/xiaomi-sdm660/android_kernel_xiaomi_sdm660.git -b master kernel/xiaomi/sdm660
+        git clone --depth=1 https://github.com/niteshkumar2000/proprietary_vendor_xiaomi.git -b ten vendor/xiaomi
+        git clone --depth=1 https://github.com/niteshkumar2000/vendor_MiuiCamera.git vendor/MiuiCamera
         rm -rf vendor/aosp/packages/overlays/NoCutoutOverlay
 	echo -e ${grn} "\n[*] Syncing sources completed! [*]" ${txtrst}
 }
