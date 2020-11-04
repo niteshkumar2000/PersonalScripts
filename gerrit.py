495a8da155df88996eea9ea7bb2c448416cf73df
Niteshkumar <nitesh156200@gmail.com>
2020-11-04 19:57:36 +0530

    gerrit: Made patchset dynamic yaay!
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- gerrit.py
+++ gerrit.py
@@ -1,4 +1,6 @@
 from pygerrit2 import GerritRestAPI, Anonymous
+import requests, json
+
 if __name__ == "__main__":
     url = 'http://freakyos.xyz'
     auth = Anonymous()
@@ -6,12 +8,15 @@ if __name__ == "__main__":
     changes = rest.get("/changes/?q=status:open")
     cherrypicks = {}
     for change in changes:
+        response = requests.get("http://freakyos.xyz/changes/FreakyOS%2F{}~{}/detail?O=916314".format(change['project'].split('/')[-1], change['_number']))
+        response = json.loads(response.text[5:])
         try:
             if not cherrypicks[change['project']]:
                 cherrypicks[change['project']] = []
         except:
             cherrypicks[change['project']] = []
-        cherrypicks[change['project']].append(("git fetch \"http://freakyos.xyz/{}\" refs/changes/{}/{}/1 && git cherry-pick FETCH_HEAD").format(change['project'], str(change['_number'])[1:], change['_number']))
+        cherrypicks[change['project']].append(("git fetch \"http://freakyos.xyz/{}\" refs/changes/{}/{}/{} && git cherry-pick FETCH_HEAD").format(
+            change['project'], str(change['_number'])[1:], change['_number'], len(response['revisions'])))
     for repo, commands in cherrypicks.items():
         for command in commands:
             print(repo, command)
