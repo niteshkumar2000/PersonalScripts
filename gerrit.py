ed19df39a186603e73b850d376a403e2f85396a1
Niteshkumar <nitesh156200@gmail.com>
2020-11-04 21:10:09 +0530

    gerrit: Execute system commands
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- gerrit.py
+++ gerrit.py
@@ -1,5 +1,6 @@
 from pygerrit2 import GerritRestAPI, Anonymous
 import requests, json
+import os
 
 if __name__ == "__main__":
     url = 'http://freakyos.xyz'
@@ -18,5 +19,11 @@ if __name__ == "__main__":
         cherrypicks[change['project']].append(("git fetch \"http://freakyos.xyz/{}\" refs/changes/{}/{}/{} && git cherry-pick FETCH_HEAD").format(
             change['project'], str(change['_number'])[1:], change['_number'], len(response['revisions'])))
     for repo, commands in cherrypicks.items():
+        repo_list = ''.join(repo.split('/')[1:]).split('_')
+        print('Entering {} directory!'.format(repo))
+        os.system('cd ' + '/'.join(repo_list))
         for command in commands:
-            print(repo, command)
+            print('Picking the change!')
+            os.system(command)
+        print('Leaving the directory and return to home page!')
+        os.system('cd  '+ '../'*len(repo_list))
